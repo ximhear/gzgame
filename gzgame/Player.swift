3a141c74bd2e1fcce8838cbd37ab4f778ac56efc
gzonelee <gzonelee@gmail.com>
2021-10-26 03:05:43 +0900

    .

---
+++ gzgame/Player.swift
@@ -0,0 +1,15 @@
+//
+//  Player.swift
+//  gzgame
+//
+//  Created by gzonelee on 2021/10/26.
+//
+
+import Foundation
+import SpriteKit
+
+class Player: SKSpriteNode {
+  // MARK: - PROPERTIES
+  
+  // MARK: - Init
+}
